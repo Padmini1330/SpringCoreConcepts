ffb2078fbd93b8c14a62507f7c2df05a1245a186
padmini v <padmini.v@perfios.com>
2021-10-31 20:22:20 +0530

    ADDED: Logger for logging messages

--- src/main/java/com/demo/springconcepts/SpringConceptsApplication.java
+++ src/main/java/com/demo/springconcepts/SpringConceptsApplication.java
@@ -1,5 +1,7 @@
 package com.demo.springconcepts;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -7,14 +9,13 @@ import org.springframework.context.ApplicationContext;
 import com.demo.springconcepts.component.Demobean;
 
 @SpringBootApplication
-public class SpringConceptsApplication {
-
-	public static void main(String[] args) {
-			
-		System.out.print("Welcome to spring concepts");
-		ApplicationContext context = SpringApplication.run(SpringConceptsApplication.class, args);
-		Demobean demobean = context.getBean(Demobean.class);
-		System.out.println("Demo Bean ="+ demobean.toString());
-	}
-
+public class SpringConceptsApplication 
+{
+	public static final Logger logger = LoggerFactory.getLogger(SpringConceptsApplication.class);
+	public static void main(String[] args) 
+	{
+			logger.debug("Welcome to spring concepts");
+			ApplicationContext context = SpringApplication.run(SpringConceptsApplication.class, args);
+			logger.debug("Checking context: ={}",context.getBean(Demobean.class));
+	}	
 }
