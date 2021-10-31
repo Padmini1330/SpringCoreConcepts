5566a4e1918d95093d82005d411d427aa695e968
padmini v <padmini.v@perfios.com>
2021-10-31 20:45:37 +0530

    ADDED: Created EmployeeBean object and setting the values

--- src/main/java/com/demo/springconcepts/SpringConceptsApplication.java
+++ src/main/java/com/demo/springconcepts/SpringConceptsApplication.java
@@ -7,15 +7,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 import com.demo.springconcepts.component.Demobean;
+import com.demo.springconcepts.component.EmployeeBean;
 
 @SpringBootApplication
 public class SpringConceptsApplication 
 {
 	public static final Logger logger = LoggerFactory.getLogger(SpringConceptsApplication.class);
+	
 	public static void main(String[] args) 
 	{
-			logger.debug("Welcome to spring concepts");
-			ApplicationContext context = SpringApplication.run(SpringConceptsApplication.class, args);
-			logger.debug("Checking context: ={}",context.getBean(Demobean.class));
-	}	
+		logger.debug("Welcome to spring concepts");
+		ApplicationContext context = SpringApplication.run(SpringConceptsApplication.class, args);
+		logger.debug("Checking context: ={}",context.getBean(Demobean.class));
+		EmployeeBean employeeBean = context.getBean(EmployeeBean.class);
+		employeeBean.setEid(110);
+		employeeBean.setEname("abcd");
+		employeeBean.showEmployeeDetails();
+	}
 }
+
